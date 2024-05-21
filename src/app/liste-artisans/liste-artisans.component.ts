bae1b420ada5dda4f224d72a80bdc30b00d2ba02
LéaKugler <leakugler2@gmail.com>
2024-05-21 15:55:20 +0200

    Add files components

---
+++ src/app/liste-artisans/liste-artisans.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-liste-artisans',
+  templateUrl: './liste-artisans.component.html',
+  styleUrl: './liste-artisans.component.scss'
+})
+export class ListeArtisansComponent {
+
+}
